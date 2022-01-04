a5e8285b556a44062539e48878498783c8d7cf56
Nuccino92 <anthnucci@hotmail.com>
2022-01-03 23:19:59 -0500

    moved routes to route folder

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Hat = require("./models/hat");
-const Shoe = require("./models/shoe");
-const UpperWear = require("./models/upperWear");
-const BottomWear = require("./models/bottomWear");
+
+const hatRoutes = require("./routes/hatRoutes");
+const shoeRoutes = require("./routes/shoeRoutes");
+const upperWearRoutes = require("./routes/upperWearRoutes");
+const bottomWearRoutes = require("./routes/bottomWearRoutes");
+
 require("dotenv").config();
 
 const app = express();
@@ -30,127 +32,10 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-app.get("/hats", (req, res) => {
-  Hat.find()
-    .then((result) => {
-      res.render("category", {
-        title: "Hats",
-        result: result,
-        path: "/hats/create",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/hats/create", (req, res) => {
-  res.render("create_hat", {
-    title: "Create",
-  });
-});
-
-app.post("/hats", (req, res) => {
-  const hat = new Hat(req.body);
-  hat
-    .save()
-    .then((result) => {
-      res.redirect("/hats");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/shoes", (req, res) => {
-  Shoe.find()
-    .then((result) => {
-      res.render("category", {
-        title: "Shoes",
-        result: result,
-        path: "/shoes/create",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/shoes/create", (req, res) => {
-  res.render("create_shoe", { title: "Create" });
-});
-
-app.post("/shoes", (req, res) => {
-  const shoe = new Shoe(req.body);
-  shoe
-    .save()
-    .then((result) => {
-      res.redirect("/shoes");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/upper-wear", (req, res) => {
-  UpperWear.find()
-    .then((result) => {
-      res.render("category", {
-        title: "Upper Wear",
-        result: result,
-        path: "/upper-wear/create",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/upper-wear/create", (req, res) => {
-  res.render("create_upperWear", { title: "Create" });
-});
-
-app.post("/upperWears", (req, res) => {
-  const upperWear = new UpperWear(req.body);
-  upperWear
-    .save()
-    .then((result) => {
-      res.redirect("/upper-wear");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/bottom-wear", (req, res) => {
-  BottomWear.find()
-    .then((result) => {
-      res.render("category", {
-        title: "Bottom Wear",
-        result: result,
-        path: "/bottom-wear/create",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/bottom-wear/create", (req, res) => {
-  res.render("create_bottomWear", { title: "Create" });
-});
-
-app.post("/bottomWears", (req, res) => {
-  const bottomWear = new BottomWear(req.body);
-  bottomWear
-    .save()
-    .then((result) => {
-      res.redirect("/bottom-wear");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+app.use("/hats", hatRoutes);
+app.use("/shoes", shoeRoutes);
+app.use("/upper-wear", upperWearRoutes);
+app.use("/bottom-wear", bottomWearRoutes);
 
 app.use((req, res) => {
   res.status(404).render("error", { title: "Error" });
